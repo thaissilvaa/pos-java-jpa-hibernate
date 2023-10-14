e31b6f0846f6f8c9ffc96c9b1b9392835732f2a5
Thais Silva <tatahcristina13@gmail.com>
2023-10-14 16:02:06 -0300

    Subindo o projeto com algumas configurações já feitas

---
+++ src/main/java/pos_java_hibernate/pos_java_hibernate/App.java
@@ -0,0 +1,13 @@
+package pos_java_hibernate.pos_java_hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
